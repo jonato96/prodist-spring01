415816efddc721f0f46b0e49be5938bad9d245f3
Jonathan <jonato96s@gmail.com>
2021-12-16 15:40:36 -0500

    Se añade DI, se modifica el archivo xml

--- src/main/java/com/programacion/servicios/OperacionesImpl.java
+++ src/main/java/com/programacion/servicios/OperacionesImpl.java
@@ -1,6 +1,11 @@
 package com.programacion.servicios;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 public class OperacionesImpl implements Operaciones{
+
+    @Autowired
+    private ServicioLog servicio;
 
     public OperacionesImpl(){
         System.out.println("Estoy en el constructor");
@@ -8,6 +13,7 @@ public class OperacionesImpl implements Operaciones{
 
     @Override
     public int sumar(int x, int y) {
+        servicio.log("Inicializando suma");
         return x+y;
     }
 }
